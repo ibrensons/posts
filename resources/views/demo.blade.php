17d62c9b5b8829519450ba8be5fc8e6525421313
arturs <arturshugo4@gmail.com>
2025-04-23 10:16:15 +0300

    test routes

---
+++ resources/views/demo.blade.php
@@ -0,0 +1,15 @@
+<h1>Title</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>{{$inputText}}</p>
+</body>
+
+</html>
